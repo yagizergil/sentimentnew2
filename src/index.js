6bfb958c04f609113dd1921b039dc48d85ceada7
yagizergil <yagizergil@gmail.com>
2023-08-14 14:25:07 +0300

    new
    new

--- src/index.js
+++ src/index.js
@@ -6,12 +6,20 @@ import './index.css';
 import App from './App2';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
