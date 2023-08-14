71e72686d5580f0952db89b07c30e8aaea8adf47
yagizergil <yagizergil@gmail.com>
2023-08-14 20:40:23 +0300

    a
    x

--- src/App2.js
+++ src/App2.js
@@ -19,6 +19,15 @@ function App() {
   const [inputText, setInputText] = useState('');
   const [data, setData] = useState([]);
 
+
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:8080',
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
   const handleSubmit = () => {
     axios.post('http://localhost:8080/predict_sentiment/', { text: inputText })
       .then((response) => {
