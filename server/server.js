1fe876c5e93bcef5497f2ca984bf2765b8a2287a
yagizergil <yagizergil@gmail.com>
2023-08-16 10:10:48 +0300

    a
    a

--- server/server.js
+++ server/server.js
@@ -3,12 +3,18 @@ const cors = require('cors');
 
 const app = express();
 
-// Genel CORS ayarları
-app.use(cors());
+// Sadece belirli bir kaynaktan gelen isteklere izin ver
+const corsOptions = {
+  origin: 'https://sentimentanalysisgl.netlify.app',
+  methods: 'GET, POST',
+  allowedHeaders: 'Content-Type, Authorization',
+};
+
+app.use(cors(corsOptions));
 
 // Diğer Express yönlendirmeleri ve işlemleri burada devam eder...
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
